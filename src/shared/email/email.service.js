8820e3d847415b5cce33851fb5e7bd3d7cd82609
Dunw1n <webrazrabotka72@gmail.com>
2026-04-26 07:47:45 +0300

    Update Backend

--- src/shared/email/email.service.js
+++ src/shared/email/email.service.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 // Настройка почтового транспорта
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: 465,
-  secure: true,
+  port: process.env.EMAIL_PORT || 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
