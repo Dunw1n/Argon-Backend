465a124fa193ef4358191c738699b987ba6f10aa
Dunw1n <k1.guliaev@yandex.ru>
2026-04-26 08:45:39 +0300

    Update email.service.js

--- src/shared/email/email.service.js
+++ src/shared/email/email.service.js
@@ -7,8 +7,8 @@ dns.setDefaultResultOrder("ipv4first");
 // Настройка почтового транспорта
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: process.env.EMAIL_PORT || 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
